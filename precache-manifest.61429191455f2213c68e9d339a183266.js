d536e767516ed97a580213eefd194696cafbb755
eg0000r <47872540+eg0000r@users.noreply.github.com>
2020-03-22 13:46:42 +0300

    Updates

---
+++ precache-manifest.61429191455f2213c68e9d339a183266.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "546a9225349362bbe9f9c2c71d81e2c2",
+    "url": "/picker/index.html"
+  },
+  {
+    "revision": "ff492bfcf744c362fcf3",
+    "url": "/picker/static/css/main.a93d7743.chunk.css"
+  },
+  {
+    "revision": "dd8a7c5fe740f1c10ba6",
+    "url": "/picker/static/js/2.0b4ea006.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/picker/static/js/2.0b4ea006.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff492bfcf744c362fcf3",
+    "url": "/picker/static/js/main.5b16fbd1.chunk.js"
+  },
+  {
+    "revision": "cb7c73a8d2e28124c8a4",
+    "url": "/picker/static/js/runtime-main.9a6b0625.js"
+  }
+]);
